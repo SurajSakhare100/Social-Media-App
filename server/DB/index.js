e0c72b693e4a66a37150e54cab577bfd748d537a
suraj sakhare <sakharesuraj10@gmail.com>
2024-08-17 19:22:15 +0530

    db issue solved

--- server/DB/index.js
+++ server/DB/index.js
@@ -6,7 +6,13 @@ dotenv.config();
 const connectDB = async () => {
     try {
         const dbURI = `${process.env.MONGODB_URL}/${DB_NAME}`;
-        const connectionInstance = await mongoose.connect(dbURI);
+        const connectionInstance = await mongoose.connect(dbURI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // Additional options for production
+            serverSelectionTimeoutMS: 5000, // Adjust as needed
+            socketTimeoutMS: 45000, // Adjust as needed
+        });
         // console.log(`\nMongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection FAILED", error);
