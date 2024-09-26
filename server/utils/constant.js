dc94c50335bee9f7179c914da3791154385a42ab
suraj sakhare <sakharesuraj10@gmail.com>
2024-09-26 17:30:26 +0530

    google auth and basic updated done

--- server/utils/constant.js
+++ server/utils/constant.js
@@ -2,7 +2,7 @@ export const DB_NAME = "socialdb";
 export const options = {
     httpOnly: true, 
     secure: true, 
-    sameSite: "strict",
+    sameSite: "Strict",
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // Cookie will expire in 3 days
     path: "/", // Ensure it's accessible across the entire app
     domain: "dev-net-backend.onrender.com", 
