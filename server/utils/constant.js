fd1424c2ce2ac19612d5ff7c6c81a51f108c2a0a
suraj sakhare <sakharesuraj10@gmail.com>
2024-09-26 17:18:59 +0530

    post feature updated

--- server/utils/constant.js
+++ server/utils/constant.js
@@ -1,8 +1,10 @@
+export const DB_NAME = "socialdb";
 export const options = {
     httpOnly: true, 
     secure: true, 
-    sameSite: "None", // Use "None" for cross-domain requests (if applicable)
+    sameSite: "strict",
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // Cookie will expire in 3 days
     path: "/", // Ensure it's accessible across the entire app
-    domain: "dev-net-backend.onrender.com", // Corrected domain
+    domain: "dev-net-backend.onrender.com", 
 };
+
